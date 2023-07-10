55aca9e37fd33b295ba61d8d9155ccd78cf08b04
sipra <sipramishra085@gmail.com>
2023-07-11 01:43:12 +0530

    [Sipra]Add:create a simple linked list

---
+++ LinkedListProblem/LinkedListData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class LinkedListData
+    {
+        public void Add()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(56);
+            list.AddFirst(30);
+            list.AddFirst(70);
+            foreach (int val in list)
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Linked List Program");
+            LinkedListData listData = new LinkedListData();
+            listData.Add();
+            Console.ReadLine();
+        }
+    }
+}
